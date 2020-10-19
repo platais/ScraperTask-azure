3cff2c4264d05fee46e98cceb660334616073c4e
platais <domain@tvnet.lv>
2020-10-19 16:42:36 +0300

    log to table added

--- ApiScraper.cs
+++ ApiScraper.cs
@@ -4,12 +4,17 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Table;
 using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Blob;
+
 using Newtonsoft.Json;
 using ScraperTask.Models;
+using CloudStorageAccount = Microsoft.Azure.Cosmos.Table.CloudStorageAccount;
 
 namespace ScraperTask
 {
@@ -30,17 +35,25 @@ namespace ScraperTask
             {
                 var response = await client.GetAsync(url);
                 var payload = response.Content.ReadAsStringAsync().Result;
-                var status = response.StatusCode;
+                var status = response.StatusCode.ToString();
+                //string "2020-10-19T10:29:22+00:00" un dateTime offset 10/19/2020 10:29:22 AM +00:00
+                var timeDateStr = JsonConvert.SerializeObject(response.Headers.Date);
+                //var TdStr = response.Headers.Date;
+
+                var SE = new StatusEntity(status, timeDateStr);
+                await WriteToTable(SE, log);
 
                 Response data = new Response();
                 data = JsonConvert.DeserializeObject<Response>(payload);
 
                 //log.LogInformation(response.ToString());
-                log.LogInformation(status.ToString());
+                //log.LogInformation(status.ToString());
                 foreach (var entry in data.Entries)
                 {
                     log.LogInformation(entry.Link);
                 }
+
+                await WriteToTable(SE, log);
                 await WriteToBlob(payload, log);
             }
             catch (HttpRequestException e)
@@ -48,9 +61,24 @@ namespace ScraperTask
                 log.LogInformation("exception: " + e);
             }
         }
+
+        //[FunctionName("postgetgetpost")]
+        //public static async Task<HttpResponseMessage> Run(
+        //[HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+        //    HttpRequestMessage req,
+        // ILogger log)
+        //{
+        //    log.LogInformation("C# HTTP trigger function processed a request.");
+        //    return null;
+        //}
+
         private static async Task WriteToBlob(string source, ILogger log)
         {
             string localPath = @"./StorageTmp/";
+            if (!Directory.Exists(localPath))
+            {
+                Directory.CreateDirectory(localPath);
+            }
             var fileName = "scraperFile" + DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss") + ".txt";
             string localFilePath = Path.Combine(localPath, fileName);
 
@@ -71,9 +99,20 @@ namespace ScraperTask
             await blob.UploadAsync(uploadFileStream);
         }
 
-        private static async Task WriteToTable(String source, ILogger log)
+        private static async Task WriteToTable(StatusEntity entity, ILogger log)
         {
+            var tableName = "Scraper";//"logTable" + DateTime.UtcNow.ToString("yyyy-MM-dd-HH-mm-ss");
+            //string storageConnectionString = AppSettings.LoadAppSettings().StorageConnectionString;
+            // Retrieve storage account information from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient(new TableClientConfiguration());
+            CloudTable table = tableClient.GetTableReference(tableName);
+            await table.CreateIfNotExistsAsync();
 
+            TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(entity);
+            //TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
+            //StatusEntity insertedEntity = result.Result as StatusEntity;
+            //return insertedEntity
         }
     }
 }
---
+++ Models/StatusEntity.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System.Net;
+
+namespace ScraperTask.Models
+{
+    public class StatusEntity : TableEntity
+    {
+        public StatusEntity() 
+        {
+        }
+
+        public StatusEntity(string status, string time)
+        {
+            PartitionKey = status;
+            RowKey = time;
+        }
+
+        public string Status { get; set; }
+        public string Time { get; set;  }
+    }
+}
