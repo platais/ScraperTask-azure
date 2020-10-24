26fd4103598c4b5559304a24ef906567fe78e19f
platais <domain@tvnet.lv>
2020-10-24 23:12:45 +0300

    table query added on http trigger

---
+++ Services/HttpTriggerTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using ScraperTask.Models;
+using ScraperTask.Services;
+using System.Threading.Tasks;
+
+namespace ScraperTask.Controllers
+{
+    public class HttpTriggerTable
+    {
+        [FunctionName("ListAllLogsFromPeriod")]
+        public static async Task ListAllLogsFromPeriod(
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "entities/")] HttpRequest req, ILogger log)
+        {
+            string periodReqStr = req.Query["date"];
+            
+            CloudTable table = await TableStorage.GetTableAsync();
+
+            var query = TableStorage.GetTableQueryByDateRange(periodReqStr);
+
+
+            foreach (StatusEntity entity in
+                await table.ExecuteQuerySegmentedAsync(query, null))
+            {
+                log.LogInformation(
+                    $"\n===============================\n" +
+                    $"Status: {entity.PartitionKey}\nDateTime: {entity.Timestamp}\nResponse: {entity.Response}\n" +
+                    $"===============================\n");
+            }
+           
+        }
+
+    }
+}
--- Services/TableStorage.cs
+++ Services/TableStorage.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using ScraperTask.Models;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 
@@ -24,8 +25,24 @@ namespace ScraperTask.Services
             var table = await GetTableAsync();
             log.LogInformation("Inserting into table:\n\t {0}\n", table.Uri);
             TableOperation insertOrMergeOperation = TableOperation.InsertOrMerge(entity);
-            TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
-            StatusEntity insertedEntity = result.Result as StatusEntity;
+            //TableResult result = await table.ExecuteAsync(insertOrMergeOperation);
+            //StatusEntity insertedEntity = result.Result as StatusEntity;
+        }
+
+        public static TableQuery<StatusEntity> GetTableQueryByDateRange(string period)
+        {
+            var rangeSt = period.Split('t');
+            //eg. 23-10-2020-23:40:00t24-10-2020-00:28:00
+            var rangeStart = rangeSt[0]; 
+            var rangeEnd = rangeSt[1]; 
+            TableQuery<StatusEntity> tQueryByDateRange = new TableQuery<StatusEntity>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("DateTimeNow", QueryComparisons.GreaterThanOrEqual, rangeStart),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("DateTimeNow", QueryComparisons.LessThanOrEqual, rangeEnd)
+                    ));
+
+            return tQueryByDateRange;
         }
     }
 }
